195b37e7be8148acf2c1ca9d264da44522c671c0
Geetha S <81910957+geethas67@users.noreply.github.com>
2021-04-13 12:58:35 +0530

    Update Exercise2.java

--- Arrays/Exercise2.java
+++ Arrays/Exercise2.java
@@ -1,42 +1,46 @@
-package arraysLab2;
-import java.util.*;
-public class Exercise2
+package lab2;
+import java.util.Scanner;
+public class Ex2 
 {
+	public static String[] sortString(String[] s)
+	{
+	int mid = 0;
+	String[] s1 = new String[s.length];
+	for(int i = 0;i<s.length; i++) {
+		for(int j = i+1;j < s.length; j++ ) {
+			if(s[i].compareTo(s[j]) > 0) {
+				String temp = s[i];
+				s[i] = s[j];
+				s[j] = temp;					
+			}
+		}
+	}
+	for(int i = 0;i<s.length;i++) {
+		if(s[i].length() % 2 == 0 ) {
+			mid = s[i].length()/2;
+		}
+		else {
+			mid = (s[i].length() + 1)/2;
+		}
+		s1[i] = s[i].substring(0,mid).toUpperCase().concat(s[i].substring(mid,s[i].length()).toLowerCase());
+		}
+	for(String S : s1)
+	{
+		System.out.println(S);
+	}
+	return s1;
+ }
 	public static void main(String[] args)
 	{
-		 int count;
-	        String temp;
-	        Scanner scan = new Scanner(System.in);
-	        System.out.print("Enter number of strings:");
-	        count = scan.nextInt();
-	        
-	        
-	        String str[] = new String[count];
-	        Scanner scan2 = new Scanner(System.in);
-	        System.out.println("Enter the Strings one by one:");
-	        for(int i = 0; i < count; i++)
-	        {
-	            str[i] = scan2.nextLine();
-	        }
-	        scan.close();
-	        scan2.close();
-	        
-	        for (int i = 0; i < count; i++)  //Sorting the strings
-	        {
-	            for (int j = i + 1; j < count; j++) { 
-	                if (str[i].compareTo(str[j])>0) 
-	                {
-	                    temp = str[i];
-	                    str[i] = str[j];
-	                    str[j] = temp;
-	                }
-	            }
-	        }
-	        
-	      System.out.print("Strings in Sorted Order:");
-	        for (int i = 0; i <= count - 1; i++) 
-	        {
-	            System.out.print(str[i] + ", ");
-	        }
-	    }
+		System.out.println("Enter the String");
+		Scanner sc = new Scanner(System.in);
+		
+	    int n = sc.nextInt();
+		String[] s = new String[n];
+		for(int i = 0; i<n;i++) {
+			s[i] = sc.next();
+		}
+		sortString(s);
+		sc.close();
+	}
 }
